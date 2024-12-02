49607f3cfc5d19341cedec972bbf8d1d6f780981
Nazar Artemenkov <bima62374@gmail.com>
2024-12-02 21:56:11 +0200

    HomeWork

---
+++ src/main/java/homaWork/Main.java
@@ -0,0 +1,10 @@
+package homaWork;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int result = a+b;
+        System.out.println(result);
+    }
+}
