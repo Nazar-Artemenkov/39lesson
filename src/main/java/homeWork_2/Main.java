c0f3127304e723e6b2b88eabc20d67e7d73a9f1b
Nazar Artemenkov <bima62374@gmail.com>
2024-12-04 00:30:16 +0200

    HomeWork 02

--- src/main/java/homeWork_2/Main.java
+++ src/main/java/homeWork_2/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int x = 2;
         int y = 2;
-        int result = x / y;
+        int result = x * y;
         System.out.println(result);
     }
 }
